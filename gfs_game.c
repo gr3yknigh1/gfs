d9b6efdddd1ac83b06eee2a0aa07a8aab903d5fd
Ilya Akkuzin <gr3yknigh1@gmail.com>
2024-09-07 22:38:06 +0300

    Move under defines .wav player (go back later)

--- gfs_game.c
+++ gfs_game.c
@@ -29,10 +29,6 @@ GameFillSoundBuffer(PlatformSoundDevice *device, PlatformSoundOutput *output, u3
 
     PlatformSoundDeviceLockBuffer(device, byteToLock, bytesToWrite, &region0, &region0Size, &region1, &region1Size);
 
-    // NOTE(ilya.a): Logicly, regions might be NULLs. Maybe except first? [2024/08/31]
-    // ASSERT_NONNULL(region0);
-    // ASSERT_NONNULL(region1);
-
     u32 region0SampleCount = region0Size / output->bytesPerSample;
     i16 *sampleOut = (i16 *)region0;
     for (u32 sampleIndex = 0; sampleIndex < region0SampleCount; ++sampleIndex) {
@@ -58,8 +54,6 @@ GameFillSoundBuffer(PlatformSoundDevice *device, PlatformSoundOutput *output, u3
     PlatformSoundDeviceUnlockBuffer(device, region0, region0Size, region1, region1Size);
 }
 
-static usize gSoundPlayerIndex = 0;
-
 static void
 GameFillSoundBufferWaveAsset(
     PlatformSoundDevice *device, PlatformSoundOutput *output, WaveAsset *waveAsset, u32 byteToLock, u32 bytesToWrite) {
@@ -67,38 +61,34 @@ GameFillSoundBufferWaveAsset(
     ASSERT_NONNULL(output);
     ASSERT_NONNULL(waveAsset);
 
+    //usize currentByteIndex = output->runningSampleIndex * output->bytesPerSample;
+
     void *region0, *region1;
     u32 region0Size, region1Size;
-    i16 *sampleOut;
+    byte *sampleOut;
 
     PlatformSoundDeviceLockBuffer(device, byteToLock, bytesToWrite, &region0, &region0Size, &region1, &region1Size);
 
     u32 region0SampleCount = region0Size / output->bytesPerSample;
-    sampleOut = (i16 *)region0;
+    sampleOut = (byte *)region0;
     for (u32 sampleIndex = 0; sampleIndex < region0SampleCount; ++sampleIndex) {
-        if (gSoundPlayerIndex > waveAsset->header.dataSize / 2) {
-            gSoundPlayerIndex = 0;
-        }
-
-        i16 *sampleValue = (i16 *)waveAsset->data + gSoundPlayerIndex;
-
-        *sampleOut = *sampleValue;
-        ++sampleOut;
-        ++gSoundPlayerIndex;
+        // if (currentByteIndex > waveAsset->header.dataSize / 2) {
+        //     output->runningSampleIndex = 0;
+        // }
+        MemoryCopy(sampleOut, (byte *)waveAsset->data + output->runningSampleIndex * output->bytesPerSample, output->bytesPerSample);
+        sampleOut += output->bytesPerSample;
+        output->runningSampleIndex++;
     }
 
     u32 region1SampleCount = region1Size / output->bytesPerSample;
-    sampleOut = (i16 *)region1;
+    sampleOut = (byte *)region1;
     for (u32 sampleIndex = 0; sampleIndex < region1SampleCount; ++sampleIndex) {
-        if (gSoundPlayerIndex > waveAsset->header.dataSize / 2) {
-            gSoundPlayerIndex = 0;
-        }
-
-        i16 *sampleValue = (i16 *)waveAsset->data + gSoundPlayerIndex;
-
-        *sampleOut = *sampleValue;
-        ++sampleOut;
-        ++gSoundPlayerIndex;
+        // if (currentByteIndex > waveAsset->header.dataSize / 2) {
+        //     output->runningSampleIndex = 0;
+        // }
+        MemoryCopy(sampleOut, (byte *)waveAsset->data + output->runningSampleIndex * output->bytesPerSample, output->bytesPerSample);
+        sampleOut += output->bytesPerSample;
+        output->runningSampleIndex++;
     }
 
     PlatformSoundDeviceUnlockBuffer(device, region0, region0Size, region1, region1Size);
@@ -112,21 +102,29 @@ GameMainloop(Renderer *renderer) {
     PlatformWindow *window = PlatformWindowOpen(&platformScratch, 900, 600, "Hello world!");
     ASSERT_NONNULL(window);
 
+#if WIP_WAV_FILE_PLAYER
     WaveAsset musicAsset;
     WaveAssetLoadResult musicLoadResult =
         WaveAssetLoadFromFile(&assetScratch, ".\\Assets\\test_music_01.wav", &musicAsset);
     ASSERT_ISOK(musicLoadResult);
 
-    // PlatformSoundOutput soundOutput = PlatformSoundOutputMake(48000);
     PlatformSoundOutput soundOutput = PlatformSoundOutputMake(musicAsset.header.freqHZ);
     soundOutput.bytesPerSample = musicAsset.header.bitsPerSample / 8;
-
+#else
+    PlatformSoundOutput soundOutput = PlatformSoundOutputMake(48000);
+#endif
 
     PlatformSoundDevice *soundDevice =
         PlatformSoundDeviceOpen(&platformScratch, window, soundOutput.samplesPerSecond, soundOutput.audioBufferSize);
+
+#if WIP_WAV_FILE_PLAYER
     GameFillSoundBufferWaveAsset(
         soundDevice, &soundOutput, &musicAsset, 0,
         soundOutput.latencySampleCount * soundOutput.bytesPerSample /* output.audioBufferSize */);
+#else
+    GameFillSoundBuffer(soundDevice, &soundOutput, 0, soundOutput.latencySampleCount * soundOutput.bytesPerSample);
+#endif
+
     PlatformSoundDevicePlay(soundDevice);
 
     u32 xOffset = 0;
@@ -170,7 +168,13 @@ GameMainloop(Renderer *renderer) {
             bytesToWrite = targetCursor - byteToLock;
         }
 
-        GameFillSoundBufferWaveAsset(soundDevice, &soundOutput, &musicAsset, byteToLock, bytesToWrite);
+        if (bytesToWrite > 0) {
+
+#if WIP_WAV_FILE_PLAYER
+#else
+            GameFillSoundBuffer(soundDevice, &soundOutput, byteToLock, bytesToWrite);
+#endif
+        }
 
         xOffset++;
         yOffset++;
