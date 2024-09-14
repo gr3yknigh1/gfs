97def9696ff510435209db637909fce878dea82c
Ilya Akkuzin <gr3yknigh1@gmail.com>
2024-09-14 11:48:19 +0300

    Import shader sources

---
+++ assets/basic.frag.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec4 f_Color;
+in vec2 f_TexCoord;
+
+uniform sampler2D u_Texture;
+
+void main()
+{
+    FragColor = texture(u_Texture, f_TexCoord);
+};
